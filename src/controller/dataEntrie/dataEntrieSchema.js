20e82f44cc7858252861dcdf5ed37bd34703896a
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-10-25 10:37:02 -0300

    resolving the negative entrie validadtion

--- src/controller/dataEntrie/dataEntrieSchema.js
+++ src/controller/dataEntrie/dataEntrieSchema.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const dataEntrieSchema = Joi.object({
     description: Joi.string().required(),
     contabilType: Joi.string().required(),
-    value: Joi.number().required(),
+    value: Joi.number().required().greater(0),
 })
 
 export default dataEntrieSchema;
