aa8fc5641caf53a77ca65aef6c93590a3426a3fa
Nivea <nivea@respondeai.com.br>
2021-10-29 19:05:03 -0300

    preparando o review

--- src/controller/dataEntrie/dataEntrieSchema.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-const dataEntrieSchema = Joi.object({
-    description: Joi.string().required(),
-    contabilType: Joi.string().required(),
-    value: Joi.number().required().greater(0),
-})
-
-export default dataEntrieSchema;
--- src/controller/signIn/validateSignIn.js
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-const siginInSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required().pattern(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/),
-})
-
-export default siginInSchema;
