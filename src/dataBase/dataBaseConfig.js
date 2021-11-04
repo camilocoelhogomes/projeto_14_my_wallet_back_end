99a381b67541dcd6675550de699e3dca526abe17
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-11-04 15:42:24 -0300

    config to go to heroku

--- src/dataBase/dataBaseConfig.js
+++ src/dataBase/dataBaseConfig.js
@@ -3,11 +3,10 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connection = new Pool({
-    user: 'postgres',
-    password: '123456',
-    host: 'localhost',
-    port: 5432,
-    database: 'projeto_14_my_wallet',
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 export default connection;
